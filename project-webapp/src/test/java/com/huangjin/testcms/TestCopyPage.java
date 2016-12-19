89051f6081bcb910f8394d33414f0b8458d0ecc7
huangjin <huangjin@le.com>
2016-12-19 15:48:09 +0800

    commit

--- project-webapp/src/test/java/com/huangjin/testcms/TestCopyPage.java
+++ project-webapp/src/test/java/com/huangjin/testcms/TestCopyPage.java
@@ -16,8 +16,8 @@ public class TestCopyPage {
     public void test1() {
         String token = Md5Util.MD5("/frame/copyAllAppPage/" + 400001);
 
-        //String url = "http://api.m.cms.lecloud.com/frame/copyAllAppPage/12345/1/" + token;
-        String url = "http://localhost:822/frame/copyAllAppPage/400001/-1/" + token;
+        String url = "http://api.m.cms.lecloud.com/frame/copyAllAppPage/12345/1/" + token;
+        //String url = "http://localhost:822/frame/copyAllAppPage/400001/-1/" + token;
 
         Map<String, String> param = new HashMap<>();
 
